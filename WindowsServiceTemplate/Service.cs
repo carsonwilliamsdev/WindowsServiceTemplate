4c6bc0061848af072de919383882f6957bf980de
Carson Williams <sxhatrix@gmail.com>
2020-01-03 21:11:21 -0700

    initial commit
    Windows Service Template

---
+++ WindowsServiceTemplate/Service.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+
+namespace WindowsServiceTemplate
+{
+    public partial class MyService : ServiceBase
+    {
+        public MyService()
+        {
+            InitializeComponent();
+
+            string MyLogSourceName = "MyServiceSource";
+            string MyLogName = "MyServiceLog";
+            if (!EventLog.SourceExists(ServiceName))
+            {
+                EventLog.CreateEventSource(
+                    MyLogSourceName, MyLogName);
+            }
+            _eventLog.Source = MyLogSourceName;
+            _eventLog.Log = MyLogName;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Update the service state to Start Pending.  
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            // Do initialization stuff here..
+
+            // Update the service state to Running.  
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            _eventLog.WriteEntry(ServiceName + " started.");
+        }
+
+        protected override void OnStop()
+        {
+            // Update the service state to Stop Pending.  
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_STOP_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            // Update the service state to Stopped.  
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_STOPPED;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            _eventLog.WriteEntry(ServiceName + " stopped.");
+        }
+
+        #region Windows Functions
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(IntPtr handle, ref ServiceStatus serviceStatus);
+
+        public enum ServiceState
+        {
+            SERVICE_STOPPED = 0x00000001,
+            SERVICE_START_PENDING = 0x00000002,
+            SERVICE_STOP_PENDING = 0x00000003,
+            SERVICE_RUNNING = 0x00000004,
+            SERVICE_CONTINUE_PENDING = 0x00000005,
+            SERVICE_PAUSE_PENDING = 0x00000006,
+            SERVICE_PAUSED = 0x00000007,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ServiceStatus
+        {
+            public uint dwServiceType;
+            public ServiceState dwCurrentState;
+            public uint dwControlsAccepted;
+            public uint dwWin32ExitCode;
+            public uint dwServiceSpecificExitCode;
+            public uint dwCheckPoint;
+            public uint dwWaitHint;
+        };
+
+        #endregion Windows Functions End
+    }
+}
